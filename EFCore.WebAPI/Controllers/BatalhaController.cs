e7e57d80570410db3b53c58f1db5b0f47cf62a6a
Rafael Aguilar Magalhães <rafaelaguilar.inspidia@outlook.com>
2020-01-10 14:43:45 -0300

    Controller Batalha

--- EFCore.WebAPI/Controllers/BatalhaController.cs
+++ EFCore.WebAPI/Controllers/BatalhaController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFCore.Dominio;
 using EFCore.Repo;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCore.WebAPI.Controllers
 {
@@ -12,8 +14,7 @@ namespace EFCore.WebAPI.Controllers
     [ApiController]
     public class BatalhaController : ControllerBase
     {
-
-        private readonly HeroiContext _context;
+        private readonly HeroiContext _context; 
 
         public BatalhaController(HeroiContext context)
         {
@@ -21,13 +22,21 @@ namespace EFCore.WebAPI.Controllers
         }
         // GET: api/Batalha
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                return Ok(new Batalha());
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest($"Erro: {ex}");
+            }
         }
 
         // GET: api/Batalha/5
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}", Name = "GetBatalha")]
         public string Get(int id)
         {
             return "value";
@@ -35,14 +44,47 @@ namespace EFCore.WebAPI.Controllers
 
         // POST: api/Batalha
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post(Batalha batalha)
         {
+            try
+            {
+                _context.Batalhas.Add(batalha);
+                _context.SaveChanges();
+                return Ok("BAZINGA");
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest("Impossível de cadastrar a batalha");
+            }
+
         }
 
         // PUT: api/Batalha/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult Put(int id, Batalha batalha)
         {
+
+            try
+            {
+                
+                if (_context.Batalhas.AsNoTracking().FirstOrDefault(h => h.Id == id) != null)
+                {
+                    //Aqui salvou essa batalha dentro das tabelas qu estão na parte EFCore.Dominio atravez do -context, que é responsavel por fazer essa conexão. E salvou essas alterações dentro do SQL utilizando o SaveChanges().
+                    _context.Batalhas.Update(batalha);
+                    _context.SaveChanges();
+                    return Ok("BAZINGA");
+                }
+
+                return Ok("Batalha não Encontrado!");
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest($"Erro: {ex}");
+            }
         }
 
         // DELETE: api/ApiWithActions/5
